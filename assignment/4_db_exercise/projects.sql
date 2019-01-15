c8cb5be0f51511343b5fdde60f7f6295b7b469f3
hirosoft40 <hirosoft40@gmail.com>
2019-01-15 13:01:03 -0600

    change

--- assignment/4_db_exercise/projects.sql
+++ assignment/4_db_exercise/projects.sql
@@ -5,91 +5,91 @@
 -- SELECT * FROM tech;
 
 -- ====== ASSIGNMENT JOIN =======
--- -- 1. What are all projects that use JavaScript?
--- SELECT * FROM project_uses_tech pu
---     INNER JOIN project p ON pu.project_id = p.id
---     INNER JOIN tech t ON pu.tech_id = t.id
---     WHERE t.name LIKE '%Script';
+-- 1. What are all projects that use JavaScript?
+SELECT * FROM project_uses_tech pu
+    INNER JOIN project p ON pu.project_id = p.id
+    INNER JOIN tech t ON pu.tech_id = t.id
+    WHERE t.name LIKE '%Script';
 
 -- -- SELECT * FROM project_uses_tech WHERE tech_id = 3;
 
 -- -- 2. What are all technologies used by the Personal Website?
--- SELECT t.name AS language FROM project_uses_tech pu
---     INNER JOIN tech t ON pu.tech_id = t.id
---     INNER JOIN project p ON pu.project_id = p.id
---     WHERE p.name = 'Personal Website';
+SELECT t.name AS language FROM project_uses_tech pu
+    INNER JOIN tech t ON pu.tech_id = t.id
+    INNER JOIN project p ON pu.project_id = p.id
+    WHERE p.name = 'Personal Website';
 
 -- -- 3. Perform a left outer join from the tech table to the project_uses_tech table - which techs has no associated project?
--- SELECT * FROM tech t 
---     LEFT OUTER JOIN project_uses_tech pu ON t.id = pu.tech_id;
+SELECT * FROM tech t 
+    LEFT OUTER JOIN project_uses_tech pu ON t.id = pu.tech_id;
 
 -- 4. Based on the previous query, get the count of the number of techs used by each project.
--- SELECT p.name, COUNT(pu.tech_id) AS num_of_tech FROM tech t
---     LEFT OUTER JOIN project_uses_tech pu ON t.id = pu.tech_id
---     JOIN project p ON p.id = pu.project_id
---     GROUP BY p.name
---     ORDER BY p.name;
+SELECT p.name, COUNT(pu.tech_id) AS num_of_tech FROM tech t
+    LEFT OUTER JOIN project_uses_tech pu ON t.id = pu.tech_id
+    JOIN project p ON p.id = pu.project_id
+    GROUP BY p.name
+    ORDER BY p.name;
 
 -- -- 5. Perform a left outer join from the project table to the project_user_tech table - which projects has no associated tech?
--- SELECT * FROM project p
---      LEFT OUTER JOIN project_uses_tech pu ON p.id = pu.project_id;
+SELECT * FROM project p
+     LEFT OUTER JOIN project_uses_tech pu ON p.id = pu.project_id;
 
 -- -- 6. Based on the previous query, get the count of the number of projects that use each tech.
--- SELECT DISTINCT(t.name), COUNT(t.id) FROM tech t
---     INNER JOIN project_uses_tech pu ON t.id = pu.tech_id
---     INNER JOIN project p ON p.id = pu.project_id
---     GROUP BY t.name;
+SELECT DISTINCT(t.name), COUNT(t.id) FROM tech t
+    INNER JOIN project_uses_tech pu ON t.id = pu.tech_id
+    INNER JOIN project p ON p.id = pu.project_id
+    GROUP BY t.name;
 
 -- 7. List all projects along with each technology used by it. You will need to do a three-way join.
--- SELECT 
---     p.name
---     , t.name
--- FROM project_uses_tech pu
--- JOIN tech t ON t.id = pu.tech_id
--- JOIN project p ON p.id = pu.project_id;
+SELECT 
+    p.name
+    , t.name
+FROM project_uses_tech pu
+JOIN tech t ON t.id = pu.tech_id
+JOIN project p ON p.id = pu.project_id;
 
 -- 8. List all the distinct techs that are used by at least one project.
--- SELECT DISTINCT(t.name) FROM project_uses_tech pu 
---     INNER JOIN project p ON p.id = pu.project_id
---     INNER JOIN tech t ON t.id = pu.tech_id
---     GROUP BY t.name
+SELECT DISTINCT(t.name) FROM project_uses_tech pu 
+    INNER JOIN project p ON p.id = pu.project_id
+    INNER JOIN tech t ON t.id = pu.tech_id
+    GROUP BY t.name
 
 -- 9. List all the distinct techs that are not used by any projects.
--- SELECT DISTINCT(t.name) FROM project_uses_tech pu 
---     RIGHT OUTER JOIN project p ON p.id = pu.project_id
---     RIGHT OUTER JOIN tech t ON t.id = pu.tech_id
--- WHERE pu.tech_id IS NULL
--- GROUP BY t.name
+SELECT DISTINCT(t.name) FROM project_uses_tech pu 
+    LEFT OUTER JOIN project p ON p.id = pu.project_id
+    RIGHT OUTER JOIN tech t ON t.id = pu.tech_id
+WHERE pu.tech_id IS NULL
+GROUP BY t.name;
 
 
 -- -- 10. List all the distinct projects that use at least one tech.
---  SELECT DISTINCT(p.name) FROM project_uses_tech pu 
---     INNER JOIN project p ON p.id = pu.project_id
---     INNER JOIN tech t ON t.id = pu.tech_id
---     WHERE pu.tech_id IS NOT NULL
---     GROUP BY p.name;
+ SELECT DISTINCT(p.name) FROM project_uses_tech pu 
+    INNER JOIN project p ON p.id = pu.project_id
+    INNER JOIN tech t ON t.id = pu.tech_id
+    WHERE pu.tech_id IS NOT NULL
+    GROUP BY p.name;
 
 
 -- -- 11. List all the distinct projects that use no tech.
---  SELECT DISTINCT(p.name) FROM project_uses_tech pu 
---     FULL OUTER JOIN project p ON p.id = pu.project_id
---     LEFT OUTER JOIN tech t ON t.id = pu.tech_id
---     WHERE t.name IS NULL
---     GROUP BY p.name;
+ SELECT DISTINCT(p.name) FROM project_uses_tech pu 
+    FULL OUTER JOIN project p ON p.id = pu.project_id
+    LEFT OUTER JOIN tech t ON t.id = pu.tech_id
+    WHERE t.name IS NULL
+    GROUP BY p.name;
 
 -- -- 12. Order the projects by how many tech it uses.
---  SELECT COUNT(t.name), p.name FROM project_uses_tech pu 
---     INNER JOIN project p ON p.id = pu.project_id
---     INNER JOIN tech t ON t.id = pu.tech_id
---     GROUP BY p.name
---     ORDER BY 1 DESC;
+ SELECT COUNT(t.name), p.name FROM project_uses_tech pu 
+    INNER JOIN project p ON p.id = pu.project_id
+    INNER JOIN tech t ON t.id = pu.tech_id
+    GROUP BY p.name
+    ORDER BY 1 DESC;
 
 -- -- 13. Order the tech by how many projects use it.
--- SELECT t.name, COUNT(p.name) FROM project_uses_tech pu
---     INNER JOIN project p ON p.id = pu.project_id
---     INNER JOIN tech t ON t.id = pu.tech_id
---     GROUP BY t.name
---     ORDER BY 2 DESC;
+SELECT t.name, COUNT(p.name) FROM project_uses_tech pu
+    INNER JOIN project p ON p.id = pu.project_id
+    INNER JOIN tech t ON t.id = pu.tech_id
+    GROUP BY t.name
+    ORDER BY 2 DESC;
 
 -- 14. What is the average number of techs used by a project?
 SELECT 
