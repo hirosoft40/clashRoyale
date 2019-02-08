9b19f5e1813a8a80c2fb6ff39d8fe5ab783252aa
hirosoft40 <hirosoft40@gmail.com>
2019-02-08 15:09:00 -0600

    change

---
+++ public/js/search.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $("#btnSearch").on('click',(e)=>{
+        $(".alert").hide();
+        // input button value check
+        if($("#inputSearch").val()){
+            $(".alert").show().val("Please type in valid entry.")
+            $(".alert").fadeTo(2000, 500).slideUp(500, function(){
+            $(".alert").slideUp(500);
+            });
+        }
+        });
+});
+        
